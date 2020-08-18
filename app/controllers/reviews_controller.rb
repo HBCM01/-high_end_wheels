3f3e3f35aa0287b2a07556e651fc305fcc739ea2
Henry Martin <henry.martin01@hotmail.com>
2020-08-18 21:09:28 +0100

    review model, controller and views created. Still a problem with create method

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,34 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    @review.booking_id = @booking
+    if @review.save
+      redirect_to my_profile_path
+    else
+      flash[:alert] = @review.errors.messages
+      render :new
+    end
+  end
+    #  def create
+    # @booking = Booking.new(booking_params)
+    # @booking.user = current_user
+    # @car = Car.find(params[:car_id])
+    # @booking.car = @car
+    # if @booking.save
+    #   redirect_to my_profile_path
+    # else
+    #   render :new
+    # end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+end
