2d5772aae02b631178798178281ac8df73ab6b2d
Henry Martin <henry.martin01@hotmail.com>
2020-08-19 12:01:43 +0100

    created a pending accepted in my profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,15 @@ class PagesController < ApplicationController
   end
 
   def my_profile
+    @pending_bookings = current_user.bookings.where(status: false)
+    @confirmed_bookings = current_user.bookings.where(status: true)
+  end
 
+  def my_booking_requests
+    @cars = current_user.cars
+    @bookings = []
+    @cars.each do |car|
+      @bookings << car.bookings
+    end
   end
 end
