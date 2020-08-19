d705a98b2577185fffcfd55c574beaca8a22d776
Henry Martin <henry.martin01@hotmail.com>
2020-08-19 14:07:14 +0100

    car show page

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,6 +6,11 @@ class Booking < ApplicationRecord
   validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
 
+  def total_price
+    @days = (end_date - start_date).to_i
+    @days * car.price
+  end
+
   private
 
   def end_date_after_start_date
@@ -14,5 +19,5 @@ class Booking < ApplicationRecord
     if end_date < start_date
       errors.add(:end_date, "must be after the start date")
     end
- end
+  end
 end
